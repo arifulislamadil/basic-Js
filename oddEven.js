b434f364139a715a7aab026b07644b25649e2bef
Arifu Islam Adil <adil.islam.arif@gmail.com>
2021-12-14 08:54:30 +0600

    even & odd number calculation

--- oddEven.js
+++ oddEven.js
@@ -1,8 +1,36 @@
-var num = 7676767867868767540800988985756;
+var num = 7676;
 var result = num % 2;
 if (result == 0) {
     console.log("This number is Even Number")
 } else {
     console.log("This number is Odd Number")
 }
+
+function isEven(numbers) {
+    if (numbers % 2 == 0) {
+        return true;
+    } else {
+        return false;
+    }
+
+
+}
+var input = 34;
+var result = isEven(input);
+console.log(result)
+
+
+function isOdd(numbers) {
+    if (numbers % 2 != 0) {
+        return true;
+    } else {
+        return false;
+    }
+
+
+}
+var input = 343;
+var result = isOdd(input);
+console.log(result)
+
 
